53af3877830d9adeb6489660ff583534d638fb9d
biranjay kumar gupta <biranjaykumargupta@gmail.com>
2024-08-12 16:35:42 +0530

    change in bannerController

--- models/bannerModel.js
+++ models/bannerModel.js
@@ -6,10 +6,8 @@ const bannerSchema = new Schema(
       type: String,
       // required: true,
     },
-    bannerImage: {
+    image: {
       type: String,
-      default:
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fin.pinterest.com%2Fpin%2Fbeauty-salon-social-media-banner-design--288511919889737115%2F&psig=AOvVaw14LPv5ZLPQIkDhFS-jjlPK&ust=1723301475663000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJD-rLSU6IcDFQAAAAAdAAAAABAE",
       required: true,
     },
     position: {
