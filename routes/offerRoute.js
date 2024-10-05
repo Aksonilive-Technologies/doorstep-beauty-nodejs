581b400e3b70136377d100916b13d56bf38e5cb7
Suraj <kumarsuraj54057@gmail.com>
2024-10-05 16:45:29 +0530

    feat: created search api for complaint and offers

--- routes/offerRoute.js
+++ routes/offerRoute.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const { createOffer, getOffers, deleteOffer, changeOfferStatus } = require("../controller/offerController");
+const {
+  createOffer,
+  getOffers,
+  deleteOffer,
+  changeOfferStatus,
+  searchOffers,
+} = require("../controller/offerController");
 const router = express.Router();
 
 router.post("/create", createOffer);
 router.get("/fetch/all", getOffers);
 router.put("/delete", deleteOffer);
 router.put("/change-status", changeOfferStatus);
+router.get("/search-offer", searchOffers);
 
 module.exports = router;
