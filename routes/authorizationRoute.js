7aa240d2184a7b2bae997d9b1843bcbef79c264a
Ashutosh Kumar Soni <ashutoshkumar1680@gmail.com>
2024-10-14 13:03:47 +0530

    send test otp added

--- routes/authorizationRoute.js
+++ routes/authorizationRoute.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 const {
   sendOTP,
+  sendTestOTP,
   verifyOTP,
   registerMasterOTP,
 } = require("../controller/authController");
 
 
 router.get("/send", sendOTP);
+router.post("/send-test", sendTestOTP);
 router.post("/verify", verifyOTP);
 router.post("/register-master-otp", registerMasterOTP);
 
