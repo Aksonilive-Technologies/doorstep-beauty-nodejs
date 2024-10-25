d08b7139cfce97d019d66502c116cc34a5ad0a29
Suraj <kumarsuraj54057@gmail.com>
2024-10-26 00:18:16 +0530

    assign-partner to booking manually

--- controller/bookingAdminController.js
+++ controller/bookingAdminController.js
@@ -1,5 +1,11 @@
+const { calculatePartnerCommission } = require("../helper/calculatePartnerCommission");
 const Booking = require("../models/bookingModel");
 const Customer = require("../models/customerModel");
+const FirebaseToken = require("../models/firebaseTokenModel");
+const CustomerFCMService = require("../helper/customerFcmService.js");
+const Partner = require("../models/partnerModel");
+const PartnerTransaction = require("../models/partnerTransactionModel");
+const { default: mongoose } = require("mongoose");
 
 exports.fetchBookings = async (req, res) => {
   try {
@@ -182,5 +188,102 @@ exports.searchBookings = async (req, res) => {
       message: "Error occurred while searching bookings",
       error: error.message,
     });
+  }
+};
+
+exports.assignPartnerToBooking = async (req, res) => {
+  const { bookingId, partnerId } = req.query;
+
+  try {
+    // Validate required fields
+    if (!partnerId || !bookingId) {
+      return res.status(400).json({
+        success: false,
+        message: "Partner ID and Booking ID are required",
+      });
+    }
+
+    // Check if the partner exists
+    let partner = await Partner.findOne({
+      _id: partnerId,
+      isDeleted: false,
+      isActive: true,
+    });
+    if (!partner) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Partner not found" });
+    }
+
+    // Find the booking by ID
+    const booking = await Booking.findOne({
+      _id: bookingId,
+      serviceStatus: "pending",
+      isDeleted: false,
+      isActive: true,
+    });
+    if (!booking) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Booking not found" });
+    }
+
+    // Get the commission percentage based on the booking price
+    const commission = calculatePartnerCommission(booking.finalPrice);
+    const minimumWalletBalance = 500 + (booking.finalPrice * (commission / 100));
+
+    // Check if partner's wallet balance is sufficient
+    if (partner.walletBalance < minimumWalletBalance) {
+      return res.status(400).json({
+        success: false,
+        message: "Insufficient wallet balance",
+      });
+    }
+
+    // Check if a partner is already assigned
+    if (booking.partner && booking.partner.length > 0) {
+      return res
+        .status(400)
+        .json({ message: "Partner is already assigned to this booking" });
+    }
+
+    // Assign partner to booking and deduct wallet balance
+    const newPartner = { partner: new mongoose.Types.ObjectId(partnerId), rating: 0 };
+    booking.partner.push(newPartner);
+    partner.walletBalance -= booking.finalPrice * (commission / 100);
+    partner.save();
+
+    // Create a transaction log for the wallet deduction
+    const transaction = new PartnerTransaction({
+      partnerId: partnerId,
+      transactionType: "booking_confirmation",
+      amount: booking.finalPrice * (commission / 100),
+      paymentGateway: "wallet",
+      status: "completed",
+    });
+    await transaction.save();
+
+    // Update booking status to processing
+    booking.status = "processing";
+    if(booking.paymentStatus === "completed"){
+      booking.serviceStatus = "scheduled";
+    }
+    await booking.save();
+
+    // Notify customer via FCM
+    const customerTokens = await FirebaseToken.find({ userId: booking.customer, userType: "customer" });
+    if (customerTokens) {
+      customerTokens.forEach(token => {
+        CustomerFCMService.sendPartnerAllocationConfirmationMessage(token.token);
+      });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Partner assigned successfully", booking });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error.message });
   }
 };
--- routes/bookingAdminRoute.js
+++ routes/bookingAdminRoute.js
@@ -5,5 +5,6 @@ const Bookings = require("../controller/bookingAdminController");
 router.get("/fetch/all", Bookings.fetchBookings);
 router.get("/download-excel", Bookings.downloadExcelSheet);
 router.get("/search-booking", Bookings.searchBookings);
+router.post("/assign-partner", Bookings.assignPartnerToBooking);
 
 module.exports = router;
