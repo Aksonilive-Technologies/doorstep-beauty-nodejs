148a4c29e6c8e0752d13396f3daf254208fe5098
Ashutosh Kumar Soni <ashutoshkumar1680@gmail.com>
2025-01-31 17:07:34 +0530

    product sorting fix-4

--- src/features/product/model/product.model.js
+++ src/features/product/model/product.model.js
@@ -10,6 +10,10 @@ const productSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        position: {
+            type: Number,
+            required: true,
+          },
         price: {
             type: Number,
             required: true,
