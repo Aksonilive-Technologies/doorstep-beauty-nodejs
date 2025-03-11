6939390a97078dfc63a723488963871ee0549cfd
Ashutosh Kumar Soni <ashutoshkumar1680@gmail.com>
2025-03-11 18:13:37 +0530

    fetch all customer offers api code updated-2

--- src/features/offers/controller/offer-customer.controller.js
+++ src/features/offers/controller/offer-customer.controller.js
@@ -6,34 +6,59 @@ export const getOfferByCode = async (req, res) => {
     return res.status(400).json({ message: "Offer code is required" });
   }
   try {
-    const offer = await Offer.findOne({ offerCode: code, isActive: true, isDeleted: false });
+    const offer = await Offer.findOne({
+      offerCode: code,
+      isActive: true,
+      isDeleted: false,
+    });
     if (!offer) {
       return res.status(404).json({ message: "Offer not found" });
     }
-    return res.status(200).json({ offer });
+    return res.status(200).json({
+      success: true,
+      message: "offer retrieved successfully",
+      data: offer,
+    });
   } catch (error) {
-    return res.status(500).json({ success: false,
+    return res
+      .status(500)
+      .json({
+        success: false,
         message: "Error fetching offer",
-        error: error.message, });
+        error: error.message,
+      });
   }
 };
 export const getOffers = async (req, res) => {
-    try {
-        const offers = await Offer.find({ isActive: true, isDeleted: false, applicableOn: { $in: [
-            "wallet_booking",
-            "package_booking",
-            "credit_card",
-            "debit_card",
-            "upi",
-          ] }}).select("-isDeleted -isActive -__v").lean();
-          offers.forEach((offer) => {
-            offer.isEligible = true;
-          }
-          );
-        return res.status(200).json({ offers });
-    } catch (error) {
-        return res.status(500).json({ success: false,
-            message: "Error fetching offers",
-            error: error.message, });
-    }
+  try {
+    const offers = await Offer.find({
+      isActive: true,
+      isDeleted: false,
+      applicableOn: {
+        $in: [
+          "wallet_booking",
+          "package_booking",
+          "credit_card",
+          "debit_card",
+          "upi",
+        ],
+      },
+    })
+      .select("-isDeleted -isActive -__v")
+      .lean();
+    offers.forEach((offer) => {
+      offer.isEligible = true;
+    });
+    return res.status(200).json({ success: true,
+        message: "offers retrieved successfully",
+        data: offers });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({
+        success: false,
+        message: "Error fetching offers",
+        error: error.message,
+      });
+  }
 };
