b7204eed5f1dec981684bfda02eb4f8b958885d4
Ashutosh Kumar Soni <ashutoshkumar1680@gmail.com>
2025-03-22 17:24:29 +0530

    .

--- src/features/product/route/product-app.route.js
+++ src/features/product/route/product-app.route.js
@@ -2,8 +2,8 @@ import express from "express";
 const router = express.Router();
 import * as Product from "../controller/product-app.controller.js"; // Update the path accordingly
 
-router.get("/fetch/new-products", Product.getAllNewProducts);
-router.get("/fetch/free-products", Product.getAllFreeProducts);
+router.get("/fetch/new", Product.getAllNewProducts);
+router.get("/fetch/free", Product.getAllFreeProducts);
 router.get("/fetch/all", Product.getAllProducts);
 
 export default router;
